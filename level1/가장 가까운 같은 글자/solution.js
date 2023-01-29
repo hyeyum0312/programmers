674a41f623769cf9b9ebbd148fa490986f1f9ed3
HYEYEON <95748753+hyeyum0312@users.noreply.github.com>
2023-01-29 16:52:01 +0900

    Add: solution

--- level1/가장 가까운 같은 글자/solution.js
+++ level1/가장 가까운 같은 글자/solution.js
@@ -1,5 +1,19 @@
 function solution(s) {
-    var answer = [];
-    console.log('??');
+    let stack = [];
+    let answer = [];
+    let arr = [...s];
+
+    arr.forEach((str) => {
+        console.log('str', str)
+        if (!stack.includes(str)) {
+            answer.push(-1);
+        }
+
+        if (stack.includes(str)) {
+            //배열의 갯수 - 배열 내 단어의 위치
+            answer.push(stack.length - stack.lastIndexOf(str));
+        }
+        stack.push(str);
+    })
     return answer;
 }
