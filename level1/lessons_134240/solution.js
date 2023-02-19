5ca5c004c1b05647227751e8646a0aa9244c04dd
조혜연 <hyeyum0312@gmail.com>
2023-02-19 22:19:57 +0900

    Add: 푸드파이트대회

--- level1/lessons_134240/solution.js
+++ level1/lessons_134240/solution.js
@@ -1,4 +1,14 @@
+// [1, 3, 4, 6]	"1223330333221"
+let food = [1, 3, 4, 6];
+solution(food);
 function solution(food) {
-    var answer = '';
-    return answer;
+    let answer = [0];
+    for(let i=food.length; i>0; i--) {
+        let foodLength = Math.floor(food[i] / 2);
+        for(let j=0; j<foodLength; j++) {
+            answer.push(i);
+            answer.unshift(i);
+        }
+    };
+    return answer.join('');
 }
